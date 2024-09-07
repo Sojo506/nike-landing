923aac0e4e1b3eae5a6ee5f12a6bf39b7b67e007
Sojo506 <fsojodev@gmail.com>
2024-09-06 20:26:16 -0600

    Creates and styles ShoeCard.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,12 @@ import Button from '../components/Button';
 import { arrowRight } from '../assets/icons';
 import { shoes, statistics } from '../constants/index';
 import { bigShoe1 } from '../assets/images';
+import ShoeCard from '../components/ShoeCard';
+import { useState } from 'react';
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id='home'
@@ -42,17 +46,23 @@ const Hero = () => {
 
       <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-center'>
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt='shoe colletion'
           width={610}
           height={502}
           className='object-contain relative z-10'
         />
 
-        <div>
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((shoe, index) => (
             <div key={index}>
-              {/* <ShowCard /> */}
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImg={(shoeImg) => {
+                  setBigShoeImg(shoeImg);
+                }}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
