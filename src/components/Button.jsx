3d348318b5f3a3d0327f83e632f7d3c6414fc88e
Sojo506 <fsojodev@gmail.com>
2024-09-09 11:31:53 -0600

    Add conditions to style the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border border-coral-red font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+        ${backgroundColor ? backgroundColor : 'bg-coral-red'}
+        ${borderColor ? borderColor : 'border-coral-red'}
+        ${textColor ? textColor : 'text-white'}
+        rounded-full`}
+    >
       {label}
       {iconURL && (
         <img src={iconURL} alt='icon' className='ml-2 rounded-full w-5 h-5' />
@@ -10,3 +22,4 @@ const Button = ({ label, iconURL }) => {
 };
 
 export default Button;
+/*  bg-coral-red  */
